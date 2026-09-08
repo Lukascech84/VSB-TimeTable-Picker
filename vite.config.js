02153214edc633f9f151a0073b6f5c0f9bf6af14
Lukáš Čech <98156446+Lukascech84@users.noreply.github.com>
2026-09-08 22:54:49 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // Tento base path je nutný pro správné načítání na GitHub Pages
-  base: '/VSB-TimeTable-Picker/',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/VSB-TimeTable-Picker/', // Zde nech svůj reálný název
 })
